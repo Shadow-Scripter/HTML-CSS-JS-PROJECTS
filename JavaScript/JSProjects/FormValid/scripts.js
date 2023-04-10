2c8c629c7b770e32c13f91b7d9456ac438068ecd
Obviously Shadow <73532236+Shadow-Scripter@users.noreply.github.com>
2023-04-10 16:47:03 +0000

    Started and Finished /JavaScript/JSProjects/FormValid file

--- JavaScript/JSProjects/FormValid/scripts.js
+++ JavaScript/JSProjects/FormValid/scripts.js
@@ -5,4 +5,21 @@ function checkFormData() {
   var tname = document.getElementById('txt_name').value;
   var temail = document.getElementById('txt_email').value;
   var tage = document.getElementById('txt_age').value;
+
+  //Access Radio Button Values
+  var tgender1 = document.getElementById('radio1').checked;
+  var tgender2 = document.getElementById('radio2').checked;
+
+  //Access Checkbox
+  var tchkbox = document.getElementById('chkbox').checked;
+
+  message += "Name: " + tname + "<br>";
+  message += "Email: " + temail + "<br>";
+  message += "Age: " + tage + "<br>";
+  message += "Male: " + tgender1 + "<br>";
+  message += "Female: " + tgender2 + "<br>";
+  message += "Agreed: " + tchkbox + "<br>";
+
+  document.getElementById('tmessage').innerHTML = message;
+  console.log(message);
 }
